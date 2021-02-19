e496e7636c890c9ef613e8c7ddd9bc39f5cbde7c
Jure Beton <49736249+bboyjure@users.noreply.github.com>
2021-02-19 16:52:04 +0100

    first commit

---
+++ src/components/Balance.js
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+const Balance = () => {
+    const { transactions } = useContext(GlobalContext)
+    const amount = transactions.map(transaction => transaction.amount)
+    
+    const total = amount.reduce((acc,item) => (acc += item), 0).toFixed(2);
+
+    return (
+        <>
+            <h4>Your Balance</h4>
+            <h1 id='balance'>{total}€</h1>
+        </>
+    )
+}
+
+export default Balance
